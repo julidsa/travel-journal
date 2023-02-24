4fe0eca7742b3451d396f9c77a2229910f27fd57
julidsa <julianadesa830@gmail.com>
2023-02-24 11:50:43 -0300

    ajustes

--- src/components/Travels.js
+++ src/components/Travels.js
@@ -8,7 +8,7 @@ export default function Travels(props) {
             <div className="top-info">
                  <img className="icon" src={Icon} alt='icon' width="18px" />
                  <p className="local" >{props.location}</p>
-                 <h4 className="maps"> <a href={props.googleMapsUrl}> View on Google Maps </a> </h4>
+                 <h4 className="maps"> <a target="_blank" ref="noopener" href={props.googleMapsUrl}> View on Google Maps </a> </h4>
             </div>
             <div className="description">
                 <h2 className="place" >{props.title}</h2>
